cdff28f70779a91c84c922bf92871fa0ee20c95e
Shankar S <shankar.s@biits.in>
2023-10-26 15:00:51 +0530

    readme file added

---
+++ clone/pages/index.js
@@ -0,0 +1,12 @@
+import VideoDesign from "../Components/Meeting/VideoDesign";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <VideoDesign />
+    </div>
+  );
+};
+
+export default page;
